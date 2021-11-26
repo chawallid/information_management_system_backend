e1a8c4b16164610472eed176ffdbbf14134bbd37
chawallid <22177340+chawallid@users.noreply.github.com>
2021-11-26 11:58:58 +0700

    database

--- information_db.sql
+++ information_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: Nov 25, 2021 at 01:24 PM
+-- Generation Time: Nov 26, 2021 at 04:58 AM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.20
 
@@ -29,22 +29,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `info_course` (
   `id` int NOT NULL,
-  `course_name` varchar(50) NOT NULL,
-  `section` int NOT NULL,
-  `credits` int NOT NULL,
-  `volume` int NOT NULL,
-  `teahcher` varchar(50) NOT NULL,
-  `department` varchar(50) NOT NULL
+  `id_course` int DEFAULT NULL,
+  `course_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `section` int DEFAULT NULL,
+  `credits` int DEFAULT NULL,
+  `volume` int DEFAULT NULL,
+  `teahcher` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `department` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `course_name_en` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `info_course`
 --
 
-INSERT INTO `info_course` (`id`, `course_name`, `section`, `credits`, `volume`, `teahcher`, `department`) VALUES
-(701101, 'ภาษาอังกฤษ', 2, 12, 3, 'ครูอังคณา', 'คณะบริหารธุรกิจ'),
-(701102, 'ภาษาอังกฤษ22', 2, 12, 3, 'ครูอังคณา123', 'คณะบริหารธุรกิจ'),
-(701103, 'ภาษาอังกฤษ3', 2, 12, 2, 'ครูอังคณา3', 'คณะบริหารธุรกิจ');
+INSERT INTO `info_course` (`id`, `id_course`, `course_name`, `section`, `credits`, `volume`, `teahcher`, `department`, `course_name_en`) VALUES
+(1, 701101, 'ภาษาอังกฤษ', 2, 12, 3, 'ครูอังคณา', 'คณะบริหารธุรกิจ', 'english'),
+(2, 701102, 'ภาษาอังกฤษ22', 2, 12, 3, 'ครูอังคณา123', 'คณะบริหารธุรกิจ', 'english22'),
+(3, 701103, 'ภาษาอังกฤษ3', 2, 12, 2, 'ครูอังคณา3', 'คณะบริหารธุรกิจ', 'english3'),
+(8, 708557, 'gggg', 4, 4455, 30, 'NULL', 'NULL', 'ststst'),
+(9, 708559, 'test2', 4, 4455, 30, 'NULL', 'NULL', 'ststst'),
+(10, 708558, 'test2', 4, 4455, 30, 'NULL', 'NULL', 'ststst');
 
 -- --------------------------------------------------------
 
@@ -69,9 +74,11 @@ CREATE TABLE `info_teachers` (
 --
 
 INSERT INTO `info_teachers` (`id_teacher`, `designation_name`, `first_name`, `middle_name`, `last_name`, `department_name`, `first_name_en`, `middle_name_en`, `last_name_en`) VALUES
-(1, 'นักวิชาการ', 'ครูอังคณา', NULL, 'คงอยู่ยง', 'คณะบริหารธุรกิจ', 'Angkana', NULL, 'Khong Yu Yong'),
-(2, 'นักวิชาการ', 'ครูอังคณา222', NULL, 'คงอยู่ยง', 'คณะบริหารธุรกิจ', 'Angkana222', NULL, 'Khong Yu Yong'),
-(3, 'นักวิชาการ', 'ครูอังคณา33', NULL, 'คงอยู่ยง', 'คณะบริหารธุรกิจ', 'Angkana33', NULL, 'Khong Yu Yong');
+(1, 'นักวิชาการ', 'ครูอังคณา', NULL, 'คงอยู่ยง', 'ภาควิชาการบัญชี', 'Angkana', NULL, 'Khong Yu Yong'),
+(2, 'นักวิชาการ', 'ครูอังคณา222', NULL, 'คงอยู่ยง', 'ภาควิชาการเงิน', 'Angkana222', NULL, 'Khong Yu Yong'),
+(3, 'นักวิชาการ', 'ครูอังคณา33', NULL, 'คงอยู่ยง', 'ภาควิชาการตลาด', 'Angkana33', NULL, 'Khong Yu Yong'),
+(4, 'นักวิขาการ2', 'ครูอังคณา455', NULL, 'คงอยู่ยง5', 'คณะบริหารธุรกิจ3', 'Angkana', NULL, 'Khong Yu Yong33'),
+(5, 'cvcc', 'ggg', 'cvc', 'hhhh', 'ภาควิชาการบัญชี', 'cvcv', 'cvcv', 'cvcv');
 
 -- --------------------------------------------------------
 
@@ -85,17 +92,18 @@ CREATE TABLE `teaching_infomation` (
   `course_name` varchar(50) NOT NULL,
   `credits` int NOT NULL,
   `section` varchar(3) NOT NULL,
-  `instructor_id` int NOT NULL
+  `instructor` json NOT NULL,
+  `date` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `teaching_infomation`
 --
 
-INSERT INTO `teaching_infomation` (`id`, `id_course`, `course_name`, `credits`, `section`, `instructor_id`) VALUES
-(1, '701101', 'ภาษาอังกฤษ', 3, '003', 1),
-(2, '701102', 'ภาษาอังกฤษ', 3, '003', 1),
-(3, '701102', 'ภาษาอังกฤษ3', 3, '010', 1);
+INSERT INTO `teaching_infomation` (`id`, `id_course`, `course_name`, `credits`, `section`, `instructor`, `date`) VALUES
+(1, '701101', 'ภาษาอังกฤษ', 3, '003', '[{\"load\": \"20\", \"name\": \"อังคณา\"}, {\"load\": \"20\", \"name\": \"อังคณา2\"}, {\"load\": \"20\", \"name\": \"อังคณา3\"}, {\"load\": \"20\", \"name\": \"อังคณา\"}, {\"load\": \"123\", \"name\": \"ครูอังคณา33(Angkana33)\"}, {\"load\": \"20\", \"name\": \"ครูอังคณา455(Angkana)\"}, {\"load\": \"20\", \"name\": \"ggg(cvcv)\"}, {\"load\": \"20\", \"name\": \"cvcvcv(cvcvc)\"}, {\"load\": \"123\", \"name\": \"ครูอังคณา222(Angkana222)\"}, {\"load\": \"\", \"name\": \"ครูอังคณา(Angkana)\"}]', '(Tue-Fri))'),
+(2, '701102', 'ภาษาอังกฤษ', 3, '003', '[{\"load\": \"20\", \"name\": \"อังคณา\"}, {\"load\": \"20\", \"name\": \"อังคณา2\"}, {\"load\": \"20\", \"name\": \"อังคณา1\"}, {\"load\": \"20\", \"name\": \"อังคณา8\"}, {\"load\": \"123\", \"name\": \"ครูอังคณา222(Angkana222)\"}, {\"load\": \"123\", \"name\": \"ครูอังคณา222(Angkana222)\"}, {\"load\": \"123\", \"name\": \"ครูอังคณา222(Angkana222)\"}, {\"load\": \"123\", \"name\": \"ครูอังคณา222(Angkana222)\"}, {\"load\": \"\", \"name\": \"ครูอังคณา(Angkana)\"}]', '(Mon - Thu)'),
+(3, '701102', 'ภาษาอังกฤษ3', 3, '010', '[{\"load\": \"20\", \"name\": \"อังคณา\"}, {\"load\": \"20\", \"name\": \"อังคณา2\"}, {\"load\": \"20\", \"name\": \"อังคณา3\"}, {\"load\": \"123\", \"name\": \"ครูอังคณา33(Angkana33)\"}, {\"load\": \"20\", \"name\": \"ครูอังคณา(Angkana)\"}]', '(Mon - Thu)');
 
 --
 -- Indexes for dumped tables
@@ -127,13 +135,13 @@ ALTER TABLE `teaching_infomation`
 -- AUTO_INCREMENT for table `info_course`
 --
 ALTER TABLE `info_course`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=701104;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `info_teachers`
 --
 ALTER TABLE `info_teachers`
-  MODIFY `id_teacher` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_teacher` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `teaching_infomation`
